ac5d5bf41d5d0aac5ab5700a098905601b8c4a1b
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2022-07-26 18:53:37 +1200

    Generate composable declarations when building

--- packages/serve/src/build/build.ts
+++ packages/serve/src/build/build.ts
@@ -2,6 +2,7 @@ import { Parcel } from "@parcel/core";
 
 import { Logger } from "../adapter/internal/logger/logger";
 import { ServeConfig } from "../serve/serve";
+import { generateComposableDeclarations } from "./code-gen/declarations/generate-composable-declarations";
 
 const createBundler = (config: ServeConfig) => {
 	const bundler = new Parcel(config?.server?.parcel as any);
@@ -15,6 +16,7 @@ export const build = async (config: ServeConfig) => {
 		const { bundleGraph, buildTime } = await bundler.run();
 		const bundles = bundleGraph.getBundles();
 
+		await generateComposableDeclarations();
 		Logger.log(`✅ Built ${bundles.length} bundles in ${buildTime}ms!`);
 		Logger.log(`Output: ./.output`);
 	} catch (error: any) {
@@ -24,12 +26,13 @@ export const build = async (config: ServeConfig) => {
 
 export const watch = async (config: ServeConfig) => {
 	const bundler = createBundler(config);
-	const subscription = await bundler.watch((err, event) => {
+	const subscription = await bundler.watch(async (err, event) => {
 		if (err) throw err;
 
 		if (event?.type === "buildSuccess") {
 			const bundles = event.bundleGraph.getBundles();
 
+			await generateComposableDeclarations();
 			Logger.log(`✅ Built ${bundles.length} bundles in ${event.buildTime}ms!`);
 			Logger.log(`Output: ./.output`);
 		} else if (event?.type === "buildFailure")
